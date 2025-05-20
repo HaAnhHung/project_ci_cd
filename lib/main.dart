6e06fcd9e4783e6f39fc344c9c42b56f5b314b4f
HungHa <hahunguetk62@gmail.com>
2025-05-20 23:24:28 +0700

    change text main

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World! First run CI/CD'),
+          child: Text('Hello World! Test ci/cd with pull request'),
         ),
       ),
     );
